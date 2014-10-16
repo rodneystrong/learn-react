569e1c1c0917494bffeb243c4818f26972684ae6
aleeex <i can't believe it's not windows>
2014-10-15 17:12:16 -0700

    added a gitignore file to stop pullin in module-cache

--- src/app.js
+++ src/app.js
@@ -2,18 +2,20 @@
 //create lines of text with edit buttons for a user to edit text
 
 //create your variable (which is pretty much a componenet)
-var theContainer = React.createClass({
-	render: function() {
-		//wtf why do i need to put a parentheses after the return keyword??
-		return (
-			<div>
-				<input type="text"/>
-			</div>
-		);
-	}
-});
+// var theContainer = React.createClass({
+// 	render: function() {
+// 		//wtf why do i need to put a parentheses after the return keyword??
+// 		return <div>
+// 					<h1>Type in the box below please</h1>
+// 					<input type="text"/>
+// 				</div>
+		
+// 	}
+// });
 
-React.renderComponent(
-  <theContainer />,
-  document.getElementById('example')
-);
+// React.renderComponent(
+//   <theContainer />,
+//   document.getElementById('example')
+// );
+
+//Trying a different app 
