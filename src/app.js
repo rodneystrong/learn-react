8ef4d642032cc82309baeb1d8bd0f5c847a60f60
alex kang <akang2@gmail.com>
2018-05-06 04:48:40 -0700

    starting fresh again. this time with create-react-app

--- src/app.js
+++ src/app.js
@@ -1,2 +1,21 @@
-/** @jsx React.DOM */
-//create lines of text with edit buttons for a user to edit text
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
