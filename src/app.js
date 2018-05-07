f45060846fc911b072920cf30d23986facebd03a
alex kang <akang2@gmail.com>
2018-05-07 03:14:06 -0700

    added Bleh component and rendered it

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Bleh from './components/testing';
 
 class App extends Component {
   render() {
@@ -11,11 +12,13 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This is where it all is
         </p>
+        <Bleh />
       </div>
     );
   }
+
 }
 
 export default App;
---
+++ src/components/testing.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Bleh = () => {
+  return (
+    <div> I'm just testing shit </div>
+  );
+}
+
+export default Bleh;
