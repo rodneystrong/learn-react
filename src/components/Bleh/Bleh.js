ceecd5b437f8fe700b9a3a476fb647f29396eadc
alex kang <akang2@gmail.com>
2018-06-22 23:58:53 -0700

    testing template strings

--- src/components/Bleh/Bleh.js
+++ src/components/Bleh/Bleh.js
@@ -3,12 +3,16 @@ import './Bleh.css';
 
 const Bleh = () => {
   const testVar = 'this is from the variable';
+  let strVar = `
+  let's see if this is any different or if ${testVar}
+  `;
   return (
     <div>
       <h2 className="Bleh-top-heading">I'm just testing shit</h2>
       <h2 className="Bleh-mid-heading">{testVar}</h2>
+      <h2 className="Bleh-mid-heading">{strVar}</h2>
     </div>
   );
-}
+};
 
 export default Bleh;
