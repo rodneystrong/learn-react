6fd00b2073f06465ca71e9762f3f2e6746d291fe
alex kang <akang2@gmail.com>
2019-05-03 17:36:26 -0700

    used the 'checked' property for the component

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 function TodoItem(props) {
   return (
     <div className="todo-item">
-      <input type="checkbox" />
+      <input type="checkbox" checked={props.todo.completed} />
       <p>{props.todo.text}</p>
     </div>
   );
