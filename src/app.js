e58fae5d587e5a1e30a7a7ddfeff5514cbafed33
alex kang <akang2@gmail.com>
2019-05-10 17:16:10 -0700

    start of practice

--- src/app.js
+++ src/app.js
@@ -1,21 +1,16 @@
-/*
-Let's practice props and mapping components on our todo list app!
+import React from "react"
 
-I've created a js file with some todos data in it, which I'm imported into this file. (Normally this data would come from an API call, not a local file). 
+// Challenge:
+// Given an incomplete class-based component without a constructor, 
+// add a constructor and initialize state to fix the broken component.
 
-Challenge: Using the array map method, render a child component for each todo item in the todosData array and pass the relevant data to it.
-*/
-
-import React from 'react';
-import TodoItem from './components/TodoItem/TodoItem';
-import todosData from './dataApi/todosData';
-
-function App() {
-  const allTodos = todosData.map(item => (
-    <TodoItem key={item.id} todo={item} />
-  ));
-
-  return <div className="todo-list">{allTodos}</div>;
+class App extends Component() {
+    return (
+        <div>
+            <h1>{this.state.name}</h1>
+            <h3>{this.state.age} years old</h3>
+        </div>
+    )    
 }
 
-export default App;
+export default App
