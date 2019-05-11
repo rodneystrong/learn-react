a16a2d0be5edd6a45d84ea62bcef9895160a8705
alex kang <akang2@gmail.com>
2019-05-10 21:18:00 -0700

    figured out state

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
-import React from "react"
+import React from 'react';
 
 // Challenge:
-// Given an incomplete class-based component without a constructor, 
+// Given an incomplete class-based component without a constructor,
 // add a constructor and initialize state to fix the broken component.
 
-class App extends Component() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'aleeeex',
+      age: 130,
+    };
+  }
+  render() {
     return (
-        <div>
-            <h1>{this.state.name}</h1>
-            <h3>{this.state.age} years old</h3>
-        </div>
-    )    
+      <div>
+        <h1>{this.state.name}</h1>
+        <h3>{this.state.age} years old</h3>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
