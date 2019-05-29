0193a35805bd9e903945063dae83a3195b71563a
alex kang <akang2@gmail.com>
2019-05-28 21:45:36 -0700

    added onChange to the TodoItem component

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 function TodoItem(props) {
   return (
     <div className="todo-item">
-      <input type="checkbox" checked={props.item.completed} />
+      <input onChange={()=>console.log('changin')} type="checkbox" checked={props.item.completed} />
       <p>{props.item.text}</p>
     </div>
   );
