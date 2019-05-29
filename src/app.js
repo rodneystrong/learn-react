cafa754e0e840ee85252a8b9c788b9a29d92bc53
alex kang <akang2@gmail.com>
2019-05-29 14:32:20 -0700

    start of review

--- src/app.js
+++ src/app.js
@@ -1,20 +1,18 @@
 import React from "react"
-import TodoItem from './components/TodoItem/TodoItem'
-import todosData from './dataApi/todosData'
 
 class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            todos: todosData
+            count: 0
         }
     }
+    
     render() {
-        let todos = this.state.todos
-        const todoItems = todos.map((item)=><TodoItem key={item.id} item={item}/>)
         return (
-            <div className="todo-list">
-                {todoItems}
+            <div>
+                <h1>{this.state.count}</h1>
+                <button>Change!</button>
             </div>
         )
     }
