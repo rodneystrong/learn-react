affb187cc426a8278f784b93f8187b7e504bc727
alex kang <akang2@gmail.com>
2019-05-28 20:19:29 -0700

    used ternary to change dispaly of text

--- src/app.js
+++ src/app.js
@@ -10,13 +10,13 @@ class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            
+            isLoggedIn: true
         }
     }
     render() {
         return (
             <div>
-                <h1>You are currently logged (in/out)</h1>
+                <h1>You are currently logged {this.state.isLoggedIn ? 'in' : 'out' }</h1>
             </div>
         )
     }
